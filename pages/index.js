c9f5b6c2c4b8b15ef98a2d23ef871b56f38f74fe
DESKTOP-8FTDRQQ\Graeff <felipegraeffm@gmail.com>
2021-01-27 10:25:23 -0300

    Final aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const BackgroundImage = styled.div`
+/* const BackgroundImage = styled.div`
   background-image: url(${db.bg});
   flex: 1;
   background-size: cover;
   background-position: center;
-`;
+`; */
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -29,30 +28,53 @@ export const QuizContainer = styled.div`
   }
 `;
 
-export default function Home(){
-  return(
-    <BackgroundImage>
+export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz Dream Theater</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Dream Theater</h1>
           </Widget.Header>
 
-          <Widget.Content>     
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>          
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diga seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
         </Widget>
         <Widget>
-        <Widget.Content>
+          <Widget.Content>
             <h1>Quizes da Galera</h1>
 
             <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>         
+          </Widget.Content>
         </Widget>
         <Footer />
-      </QuizContainer>      
-      <GitHubCorner projectUrl="https://github.com/fgraeffm"/>
-    </BackgroundImage>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/fgraeffm" />
+    </QuizBackground>
   );
 }
-
